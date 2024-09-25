5dfa69c08959fee93fc1780380caa4b5c9898107
cjephuneh <calebjephuneh@gmail.com>
2024-09-25 14:37:46 +0300

    refactor: Optimize image loading in Hero component

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -238,17 +238,17 @@ function Hero() {
           </div>
           <div className="mt-10 flex items-center gap-x-5">
             <div className="flex -space-x-2">
-              <Image
+              <img
                 className="inline-block size-8 rounded-full ring-2 ring-white"
                 src="https://images.unsplash.com/photo-1568602471122-7832951cc4c5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=2&w=320&h=320&q=80"
                 alt="Avatar"
               />
-              <Image
+              <img
                 className="inline-block size-8 rounded-full ring-2 ring-white"
                 src="https://images.unsplash.com/photo-1531927557220-a9e23c1e4794?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=2.5&w=320&h=320&q=80"
                 alt="Avatar"
               />
-              <Image
+              <img
                 className="inline-block size-8 rounded-full ring-2 ring-white"
                 src="https://images.unsplash.com/photo-1541101767792-f9b2b1c4f127?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=3&w=320&h=320&q=80"
                 alt="Avatar"
